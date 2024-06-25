3e72250fe82c33a846fd1c59a5e4b1b244b21161
Román Martínez <rgmf@riseup.net>
2024-06-25 10:57:32 +0200

    Database backup script: this script can be used to create a cron task to make copies and it maintains 30 copies at least.

---
+++ db_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COUNT=30
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ORIGIN_DIR="$DIR/backend/bl-mariadbdata"
+BACKUP_DIR="$DIR/backup"
+DEST_DIR="$BACKUP_DIR/bl-mariadbdata_$(date +%Y%m%d)"
+
+cp -rf "$ORIGIN_DIR" "$DEST_DIR"
+
+num_copies=$(ls -1d "$BACKUP_DIR"/bl-mariadbdata_* | wc -l)
+if [ "$num_copies" -ge "$COUNT" ]; then
+    oldest_copy=$(ls -1rd "$BACKUP_DIR"/bl-mariadbdata_* | tail -n 1)
+    rm -rf "$oldest_copy"
+fi
